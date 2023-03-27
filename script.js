eeb868956af3459f00d7e0585c969b09a078bb34
Elizabeth Karr <e.karr93@icloud.com>
2023-03-27 15:08:58 -0500

    ensure passwords match

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const lastName = document.querySelector("#last-name");
 const email = document.querySelector("#email");
 const phoneNumber = document.querySelector("#phone-number");
 
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-pswd");
+const errorMessage = document.querySelector(".error-msg");
+
 firstName.addEventListener('invalid', () => {
     firstName.setCustomValidity('Please enter your first name.');
 });
@@ -17,4 +21,16 @@ email.addEventListener('invalid', () => {
 
 phoneNumber.addEventListener('invalid', () => {
     phoneNumber.setCustomValidity('Please enter a valid phone number.');
+});
+
+confirmPassword.addEventListener('blur', () => {
+    if (password.value !== confirmPassword.value) {
+        password.classList.add('error');
+        confirmPassword.classList.add('error');
+        errorMessage.classList.add('no-match');
+    } else {
+        password.classList.remove('error');
+        confirmPassword.classList.remove('error');
+        errorMessage.classList.remove('no-match');
+    }
 });
