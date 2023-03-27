5bc64fb5528b14604b89a69e640984fa2548cf21
Elizabeth Karr <e.karr93@icloud.com>
2023-03-27 16:21:50 -0500

    add script for validating password

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ const errorMessage = document.querySelector(".error-msg");
 
 const passwordList = document.querySelector("#message");
 
+const pswdLowercase = document.querySelector("#letter");
+const pswdCapital = document.querySelector("#capital");
+const pswdNumber = document.querySelector("#number");
+const pswdSpecialChar = document.querySelector("#special-char");
+const pswdLength = document.querySelector("#length");
+
 firstName.addEventListener('invalid', () => {
     firstName.setCustomValidity('Please enter your first name.');
 });
@@ -43,4 +49,50 @@ password.addEventListener('focus', () => {
 
 password.addEventListener('blur', () => {
     passwordList.style.display = "none";
+});
+
+password.addEventListener('keyup', () => {
+    let lowerCaseLetters = /[a-z]/g;
+    if(password.value.match(lowerCaseLetters)) {
+        pswdLowercase.classList.remove('pswd-invalid');
+        pswdLowercase.classList.add('pswd-valid');
+    } else {
+        pswdLowercase.classList.add('pswd-invalid');
+        pswdLowercase.classList.remove('pswd-valid');
+    }
+
+    let upperCaseLetters = /[A-Z]/g;
+    if(password.value.match(upperCaseLetters)) {
+        pswdCapital.classList.remove('pswd-invalid');
+        pswdCapital.classList.add('pswd-valid');
+    } else {
+        pswdCapital.classList.add('pswd-invalid');
+        pswdCapital.classList.remove('pswd-valid');
+    }
+
+    let specialChars = /[-’/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]/g;
+    if(password.value.match(specialChars)) {
+        pswdSpecialChar.classList.remove('pswd-invalid');
+        pswdSpecialChar.classList.add('pswd-valid');
+    } else {
+        pswdSpecialChar.classList.add('pswd-invalid');
+        pswdSpecialChar.classList.remove('pswd-valid');
+    }
+
+    let numbers = /[0-9]/g;
+    if(password.value.match(numbers)) {
+        pswdNumber.classList.remove('pswd-invalid');
+        pswdNumber.classList.add('pswd-valid');
+    } else {
+        pswdNumber.classList.add('pswd-invalid');
+        pswdNumber.classList.remove('pswd-valid');
+    }
+
+    if(password.value.length >= 8) {
+        pswdLength.classList.remove('pswd-invalid');
+        pswdLength.classList.add('pswd-valid');
+    } else {
+        pswdLength.classList.add('pswd-invalid');
+        pswdLength.classList.remove('pswd-valid');
+    }
 });
